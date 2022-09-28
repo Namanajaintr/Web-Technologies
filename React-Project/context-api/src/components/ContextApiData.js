65cbcdbd3e28f5e338ff2e111fe3852bf0a73eec
Namana Jain <86921512+Namanajaintr@users.noreply.github.com>
2022-09-28 11:23:19 +0530

    Delete React-Project directory

--- React-Project/context-api/src/components/ContextApiData.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const ContextData = createContext();
-const DataProvider = ContextData.Provider; //Component
-const DataConsumer = ContextData.Consumer; //Component
-
-export default ContextData; //default component export
-export { DataProvider, DataConsumer }; //named component export
--- React-Project/context-api/src/components/ContextApiUser.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const ContextUser = createContext();
-const UserProvider = ContextUser.Provider; //Component
-const UserConsumer = ContextUser.Consumer; //Component
-
-export default ContextUser; //default component export
-export { UserProvider, UserConsumer }; //named component export
